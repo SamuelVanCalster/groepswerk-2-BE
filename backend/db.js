841d88988c1c08715a340468b585a4be3b0156e1
phiebs02 <phiebs.d@gmail.com>
2024-05-29 15:31:06 +0200

    with env file for save hosting

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connect = async () => {
   const conn = await mysql.createPool({
-    host: "ID396978_VraagVanDeDag.db.webhosting.be",
-    user: "ID396978_VraagVanDeDag",
-    password: "S8v58Ty768PZR210X063",
-    port: 3306,
-    database: "ID396978_VraagVanDeDag",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
   });
   return conn;
 };
